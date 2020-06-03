072710646272e0f732cf3d05ccd1e929bc473694
jkshareef <jamalkshareef@gmail.com>
2020-06-03 11:06:43 -0700

    First commit

---
+++ formless-api/src/main/java/io/jamalshareef/FormlessApiApplication.java
@@ -0,0 +1,13 @@
+package io.jamalshareef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormlessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormlessApiApplication.class, args);
+	}
+
+}
---
+++ formless-api/src/test/java/io/jamalshareef/FormlessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jamalshareef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormlessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
